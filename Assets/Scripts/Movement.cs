b30d6cbedb8b4bd0906159ba99c7e2a927279d03
Kevork <kevorkchristian@gmail.com>
2021-09-21 18:47:03 +0200

    Init

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical",   movement.y);
+        animator.SetFloat("Magnitude",  movement.magnitude);
+        
+        transform.position = transform.position + movement * Time.deltaTime;
+    }
+}
