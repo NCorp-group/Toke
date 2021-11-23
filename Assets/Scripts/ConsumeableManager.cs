250be6a07b46b3b95b7f6507732a03003bf2c81f
JnsJensen <jens.jens@live.dk>
2021-11-23 17:48:51 +0100

    feature(hud): penningar consumeable

---
+++ Assets/Scripts/ConsumeableManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ConsumeableManager : MonoBehaviour
+{
+    [SerializeField] private int penningar_amount = 0;
+    private int old_penningar_amount;
+
+    [SerializeField] private GameObject penningar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        old_penningar_amount = penningar_amount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (penningar_amount != old_penningar_amount)
+        {
+            penningar.GetComponent<TextMeshProUGUI>().text = penningar_amount.ToString();
+        }
+    }
+}
