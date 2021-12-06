07d75083718fb6b9ac171f45637adc03d3e201eb
JensHJ <jens.jens@live.dk>
2021-12-07 00:19:02 +0100

    sorry forgot to add

---
+++ Assets/Scripts/PlayerHealthController.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using System;
+
+public class PlayerHealthController : MonoBehaviour
+{
+
+    public static event Action<int, int> OnPlayerHealthChange;
+    public static event Action OnPlayerDie;
+
+    public int maxHealth = 100;
+    public int currentHealth = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        OnPlayerHealthChange?.Invoke(currentHealth, maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void HealthPickup(int pickupAmount)
+    {
+        maxHealth += pickupAmount;
+        OnPlayerHealthChange?.Invoke(currentHealth, maxHealth);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        Debug.LogWarning(currentHealth);
+
+        if (currentHealth < 0)
+        {
+            currentHealth = 0;
+        }
+        else if (currentHealth >= maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+
+        OnPlayerHealthChange?.Invoke(currentHealth, maxHealth);
+
+        if (currentHealth <= 0)
+        {
+            OnPlayerDie?.Invoke();
+        }
+    }
+}
