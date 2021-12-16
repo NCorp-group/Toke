f493d6e364512667cd2fd4ad7fe4d0d322580c07
Kristoffer Sørensen <kristoffer.pbs@gmail.com>
2021-12-16 14:55:13 +0100

    feat(ai): made the AI even smarter! merge: merge

--- Assets/EnemyAI.cs
+++ Assets/EnemyAI.cs
@@ -13,6 +13,8 @@ public class EnemyAI : MonoBehaviour
 
     [Header("The point from which the distance is measured to the target. If none, then the objects transform is used")]
     [SerializeField] private Transform from;
+
+    private AIPath _ai_path;
     
     public enum EnemyType
     {
@@ -33,6 +35,9 @@ public class EnemyAI : MonoBehaviour
     
     private bool RangedBehaviour()
     {
+        var a = 2;
+        Assert.IsNotNull(target, "target != null");
+        Assert.IsNotNull(_rangedAttack, "_rangedAttack != null");
         var clear_line_of_sight = _rangedAttack.Fire(
             target,
             only_if_clear_line_of_sight: true,
@@ -69,34 +74,36 @@ public class EnemyAI : MonoBehaviour
         switch (enemyType)
         {
             case EnemyType.Ranged:
-                _behaviour = RangedBehaviour;
                 _rangedAttack = GetComponent<RangedAttack>();
                 Assert.IsNotNull(_rangedAttack);
+                Debug.Log("ranged attack is NOT null");
+                _behaviour = RangedBehaviour;
                 break;
             case EnemyType.Melee:
-                _behaviour = MeleeBehaviour;
                 _meleeAttack = GetComponent<MeleeAttack>();
                 Assert.IsNotNull(_meleeAttack);
+                _behaviour = MeleeBehaviour;
                 break;
         }
         Assert.IsNotNull(_behaviour);
 
         from = transform;
+        _ai_path = GetComponent<AIPath>();
+        Assert.IsNotNull(_ai_path, "_ai_path != null");
         // from = from ?? transform;
     }
 
-    private void SetAiState(bool state) => GetComponent<AIPath>().canMove = state;
+    private void SetAiState(bool state) => _ai_path.canMove = state;
     private void EnableAI() => SetAiState(true);
     private void DisableAI() => SetAiState(false);
-
-    public AIPath aiPath;
+    
     
     
     // Update is called once per frame
     void Update()
     {
         //Debug.Log($"DESIRED VELOCITY = {aiPath.desiredVelocity}");
-        var ai_enabled = aiPath.canMove == true;
+        var ai_enabled = _ai_path.canMove == true;
         /*if (ai_enabled)
         {
             Debug.Log($"steering target.y = {aiPath.steeringTarget.y}");
@@ -104,26 +111,16 @@ public class EnemyAI : MonoBehaviour
         }*/
         transform.localScale = new Vector3(
             ai_enabled
-                ? ((aiPath.steeringTarget - from.position).x >= 0.01 ? 1 : -1)
+                ? ((_ai_path.steeringTarget - from.position).x >= 0.01 ? 1 : -1)
                 : ((target.position - from.position).x >= 0.01 ? 1 : -1),
             1,
             1
         );
         
-        /*if (aiPath.desiredVelocity.x >= 0.01f)
-        {
-            Debug.Log("FACE RIGHT");
-            transform.localScale = new Vector3(1f, 1f, 1f);
-        }
-        else if (aiPath.desiredVelocity.x <= -0.01f)
-        {
-            Debug.Log("FACE LEFT");
-            transform.localScale = new Vector3(-1f, 1f, 1f);
-        }*/
-        
         var distance_to_target = UnityEngine.Vector2.Distance(from.position, target.position);
         var target_within_aggression_range = distance_to_target <= aggressionRadius;
         // Debug.Log($"distance to target = {distance_to_target}");
+        //if (target_within_aggression_range && _behaviour.Invoke() && !_ai_path.canMove)
         if (target_within_aggression_range && _behaviour.Invoke())
         {
             //Debug.Log("target within distance, enabling attack behaviour");
