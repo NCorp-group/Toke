f6f8cc9536f82f13e736c5f4971e916d7547398b
Kristoffer Sørensen <kristoffer.pbs@gmail.com>
2021-12-21 07:30:19 +0100

    feat(boss): change room layout when boss spawned, and boss defeated

---
+++ Assets/Scripts/SpawnObjectsOnTargetEnter.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class SpawnObjectsOnTargetEnter : MonoBehaviour
+{
+    public event Action OnObjectsSpawned;
+
+    public Transform target;
+    public LayerMask targetLayerMask;
+
+    public Transform spawnPoint;
+    public Transform[] objectsToSpawn;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log($"Something entered area with name {other.gameObject.name}");
+        
+        if (other.gameObject.name == target.gameObject.name)
+        {
+            
+            foreach (var t in objectsToSpawn)
+            {
+                Instantiate(t, spawnPoint.position, Quaternion.identity);
+            }
+            
+            OnObjectsSpawned?.Invoke();
+            
+            Destroy(gameObject);
+        }
+    }
+}
